0ca227cca791afd4668b3388e3b88ad4e3c9a787
Atsushi Igarashi <igarashi@kuis.kyoto-u.ac.jp>
2021-07-22 22:49:21 +0900

    Add test code (tz now well typed)

--- src/main.ml
+++ src/main.ml
@@ -3,7 +3,14 @@ open Trans
 
 let rec read_eval_print () =
   let code = Parser.toplevel Lexer.main (Lexing.from_channel stdin) in
-  print_string (string_of_exp (exp_of_code code));
+  let exp = string_of_exp (exp_of_code code) in
+  print_string "type ('a, 'b) either = Left of 'a | Right of 'b\n";
+  print_string "let car (x, y) = x\n";
+  print_string "let cdr (x, y) = y\n";
+  print_string "let plus (x, y) = x + y\n";
+  print_string "let mult (x, y) = x * y\n";
+  print_string "let main param_st =\n";
+  print_string exp;
   print_newline ()
 
 let _ = read_eval_print ()
