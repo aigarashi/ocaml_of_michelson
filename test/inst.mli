959c1f0ced99088d30d271e1f7376e3cd113f9c8
igarashi <igarashi@kuis.kyoto-u.ac.jp>
2021-07-24 11:56:26 +0900

    Add some more instructions randomly

--- test/inst.mli
+++ test/inst.mli
@@ -22,6 +22,8 @@ val not : 'a -> 'a
 
 (* operations on integers and natural numbers *)
 (* int and nat are identified *)
+type nat = int
+
 val neg : int -> int
 val abs : int -> int
 val isnat : int -> int option
@@ -42,3 +44,46 @@ val slice : nat -> nat -> string -> string option
 val car : 'a * 'b -> 'a
 val cdr : 'a * 'b -> 'b
 
+
+(* Domain specific data types *)
+type timestamp = int (* for simplicity *)
+type mutez = int     (* for simplicity *)
+type address
+type 'a contract
+type operation
+type key
+type key_hash
+type signature
+type chain_id
+type bls12_381_g1
+type bls12_381_g2
+type bls12_381_fr
+(* 
+sapling_transaction ms: A Sapling transaction
+sapling_state ms: A Sapling state
+ticket (t)
+ *)
+
+val transfer_tokens : 'a -> mutez -> 'a contract -> operation
+val set_delegate : key_hash option -> operation
+val balance : unit -> mutez
+val address : 'a contract -> address
+val contract : address -> 'a contract option
+val source : unit -> address
+val sender : unit -> address
+val self : unit -> 'a contract
+val self_address : unit -> address
+val amount : unit -> mutez
+val implicit_account : key_hash -> unit contract
+val voting_power : key_hash -> nat
+val now : unit -> timestamp
+val chain_id : unit -> chain_id
+val level : unit -> nat
+val total_voting_power : unit -> nat
+
+(* operations on bytes *)
+type bytes
+
+(* Cryptographic primitives *)
+val hash_key : key -> key_hash
+val blake2b : bytes -> bytes
