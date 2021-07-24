fb0bd2b260b338e31dc40b9927c0ea581c387455
igarashi <igarashi@kuis.kyoto-u.ac.jp>
2021-07-24 11:39:02 +0900

    Operations on int and string
      * LSL, LSR, CONCAT, SIZE, SLICE

--- test/inst.mli
+++ test/inst.mli
@@ -1,9 +1,6 @@
 type ('a, 'b) either = Left of 'a | Right of 'b
 type never
 
-val car : 'a * 'b -> 'a
-val cdr : 'a * 'b -> 'b
-
 (* generic comparison *)
 val eq : int -> bool
 val ge : int -> bool
@@ -14,13 +11,14 @@ val neq : int -> bool
 
 (* operations on unit *)
 val unit : unit -> unit
-val compare : 'a -> 'a -> bool
+val compare : 'a -> 'a -> int
 
 (* operations on booleans *)
-val or_ : bool -> bool -> bool (* 'or' is an OCaml keyword! *)
-val and_ : bool -> bool -> bool (* 'and' is an OCaml keyword! *)
-val xor : bool -> bool -> bool
-val not : bool -> bool
+(* they are overloaded with operations on ints, hence polymorphic *)
+val or_ : 'a -> 'a -> 'a (* 'or' is an OCaml keyword! *)
+val and_ : 'a -> 'a -> 'a (* 'and' is an OCaml keyword! *)
+val xor : 'a -> 'a -> 'a
+val not : 'a -> 'a
 
 (* operations on integers and natural numbers *)
 (* int and nat are identified *)
@@ -32,3 +30,15 @@ val add : int -> int -> int
 val sub : int -> int -> int
 val mul : int -> int -> int
 val ediv : int -> int -> (int * int) option
+val lsl_ : int -> int -> int (* keyword *)
+val lsr_ : int -> int -> int (* keyword *)
+
+(* operations on strings *)
+val concat : string -> string -> string
+val size : string -> int
+val slice : nat -> nat -> string -> string option
+
+(* Operations on pairs and right combs *)
+val car : 'a * 'b -> 'a
+val cdr : 'a * 'b -> 'b
+
